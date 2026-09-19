5c8def975a006792ad5916943ce29d066ffd390d
amanjuniorxd-wq <amanjuniorxd@gmail.com>
2026-09-19 13:30:48 +0530

    Add macOS Electron shell

---
+++ electron/main.cjs
@@ -0,0 +1,8 @@
+const { app, BrowserWindow } = require("electron");
+function createWindow() {
+  const win = new BrowserWindow({ width:1280,height:820,minWidth:980,minHeight:680,title:"GYAN VPN",webPreferences:{contextIsolation:true,nodeIntegration:false} });
+  win.loadURL("http://localhost:3000");
+}
+app.whenReady().then(createWindow);
+app.on("window-all-closed",()=>{if(process.platform!=="darwin")app.quit()});
+app.on("activate",()=>{if(BrowserWindow.getAllWindows().length===0)createWindow()});
